d8f15a8d2c177e01f06314bb4eed6986c018b1c6
redward <redwardweb@gmail.com>
2020-10-29 20:46:29 +0100

    ADD: working Scheduled async rest with max pool 1: 1petition runner each time

--- AsyncSchedulerRest5/src/main/java/com/sbtutorial/schedulerAsync/AsyncSchedulerRest5Application.java
+++ AsyncSchedulerRest5/src/main/java/com/sbtutorial/schedulerAsync/AsyncSchedulerRest5Application.java
@@ -1,9 +1,13 @@
 package com.sbtutorial.schedulerAsync;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,6 +16,20 @@ public class AsyncSchedulerRest5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncSchedulerRest5Application.class, args);
+	}
+	
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor= new ThreadPoolTaskExecutor();
+		/*note the 1 thread*/
+		executor.setCorePoolSize(1);
+		executor.setMaxPoolSize(1);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		
+		return executor;
+		
 	}
 
 }
