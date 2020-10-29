0c30593f7bda72f36183199f4e5313fcf114b998
redward <redwardweb@gmail.com>
2020-10-29 18:24:53 +0100

    Add: basic working scheduler tasks

---
+++ scheduler4/src/test/java/com/sbtutorial/scheduler/Scheduler4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduler4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
