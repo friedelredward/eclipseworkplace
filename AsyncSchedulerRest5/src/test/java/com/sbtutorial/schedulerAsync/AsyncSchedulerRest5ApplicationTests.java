90f71d1cdc0ab5c5bfb466c7937b5a28d9eee118
redward <redwardweb@gmail.com>
2020-10-29 18:29:35 +0100

    ADD: ScedulingAsync project== AsyncSchedulerRest5 based on -starter-web

---
+++ AsyncSchedulerRest5/src/test/java/com/sbtutorial/schedulerAsync/AsyncSchedulerRest5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.schedulerAsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSchedulerRest5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
