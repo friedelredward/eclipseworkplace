995717fe091837ac6d1a63088f964fee7bddde18
redward <redwardweb@gmail.com>
2020-10-30 11:59:02 +0100

    ADD: ProxiesAndRoutingProxy proj with zuul and starter-web... the real Proxy for the API(previus project)

--- ProxysAndRouting/src/main/java/com/sbtutorial/proxy/ProxysAndRoutingApplication.java
+++ ProxysAndRouting/src/main/java/com/sbtutorial/proxy/ProxysAndRoutingApplication.java
@@ -2,10 +2,23 @@ package com.sbtutorial.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ProxysAndRoutingApplication {
 
+	@RequestMapping(value="/enpoint1")
+	public String enpoint1() {
+		return "<h2 style='color:tomato'>Hellow from enpoint1</h2>";
+	}
+	
+	@RequestMapping(value="/enpoint2")
+	public String enpoint2() {
+		return "<h2 style='color :violet'>Hellow from enpoint2</h2>";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProxysAndRoutingApplication.class, args);
 	}
