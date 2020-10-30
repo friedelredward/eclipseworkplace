08558967a0ba4e771fd6a3856aa62d17f52dc7a8
redward <redwardweb@gmail.com>
2020-10-30 11:39:06 +0100

    Add: routin project + proxys. WE MAKE APP MAIN ENDPOINT entry for our API

---
+++ ProxysAndRouting/src/main/java/com/sbtutorial/proxy/ProxysAndRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxysAndRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxysAndRoutingApplication.class, args);
+	}
+
+}
