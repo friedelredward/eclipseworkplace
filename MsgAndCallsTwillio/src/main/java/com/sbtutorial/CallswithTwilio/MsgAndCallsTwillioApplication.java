1b4e641e981dba879bbfdf4e2cb2570efae151d5
redward <redwardweb@gmail.com>
2020-10-30 14:12:53 +0100

    Add: Twilio Skeleton SDK

---
+++ MsgAndCallsTwillio/src/main/java/com/sbtutorial/CallswithTwilio/MsgAndCallsTwillioApplication.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.CallswithTwilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgAndCallsTwillioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgAndCallsTwillioApplication.class, args);
+	}
+
+}
